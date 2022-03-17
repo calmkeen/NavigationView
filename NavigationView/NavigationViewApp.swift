2476df3d578c0df2d9678204afd5085ae2a5ae17
calmkeen <iyunseong159@gmail.com>
2022-03-17 17:28:16 +0900

    Initial Commit

---
+++ NavigationView/NavigationViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationViewApp.swift
+//  NavigationView
+//
+//  Created by calmkeen on 2022/03/17.
+//
+
+import SwiftUI
+
+@main
+struct NavigationViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
